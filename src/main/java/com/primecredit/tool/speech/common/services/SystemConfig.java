6018406ba552b1cd8a46d936aa150a67a6194b33
maxwellyuen <maxwellyuen@192.168.19.116>
2017-08-01 20:05:33 +0800

    Change-Id: I905fcf39bf69a59c726be85bf9f3284ae792f4c6

--- src/main/java/com/primecredit/tool/speech/common/services/SystemConfig.java
+++ src/main/java/com/primecredit/tool/speech/common/services/SystemConfig.java
@@ -11,6 +11,8 @@ public class SystemConfig {
 	private static String DICTIONARY_PATH = SOURCE_PATH + "dict";
 	private static String DICTIONARY_SINGLE_WORD_PATH = DICTIONARY_PATH + File.separatorChar + "single";
 	private static String DICTIONARY_MIX_WORD_PATH = DICTIONARY_PATH + File.separatorChar + "mix";
+	private static final String WORKING_PATH = "/Users/maxwellyuen/Documents/samples/temp/";
+	private static final String WAV_SOURCE_PATH = "/Users/maxwellyuen/Documents/samples/";
 	private static boolean DEBUG = false;
 	
 	public String getSourcePath(){
@@ -27,5 +29,13 @@ public class SystemConfig {
 	
 	public boolean isDebugMode(){
 		return DEBUG;
+	}
+	
+	public String getWorkingPath() {
+		return WORKING_PATH;
+	}
+	
+	public String getWavSourcePath() {
+		return WAV_SOURCE_PATH;
 	}
 }
--- src/main/java/com/primecredit/tool/speech/dict/repositories/WordRepository.java
+++ src/main/java/com/primecredit/tool/speech/dict/repositories/WordRepository.java
@@ -1,6 +1,5 @@
 package com.primecredit.tool.speech.dict.repositories;
 
-import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
 import org.springframework.data.repository.query.Param;
 
